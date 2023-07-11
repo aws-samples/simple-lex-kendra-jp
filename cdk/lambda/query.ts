9b26769b8ed35bd0d5ae9d703dbbb0dbcbcfc1e9
Yusuke Wada <wadaysk@amazon.co.jp>
2023-07-11 11:51:32 +0900

    add filter to Lambda

--- cdk/lambda/query.ts
+++ cdk/lambda/query.ts
@@ -1,5 +1,10 @@
 import * as lambda from 'aws-lambda';
-import { KendraClient, QueryCommand } from '@aws-sdk/client-kendra';
+import {
+  AttributeFilter,
+  DocumentAttributeValueType,
+  KendraClient,
+  QueryCommand,
+} from '@aws-sdk/client-kendra';
 
 const INDEX_ID = process.env.INDEX_ID;
 
@@ -7,6 +12,13 @@ exports.handler = async (
   event: lambda.APIGatewayProxyEvent
 ): Promise<lambda.APIGatewayProxyResult> => {
   const query = JSON.parse(event.body!).query;
+  const filters:
+    | {
+        attributeKey: string;
+        attributeType: DocumentAttributeValueType;
+        value: (string | number | Date)[];
+      }[]
+    | undefined = JSON.parse(event.body!).filters;
 
   if (!query) {
     return {
@@ -19,18 +31,85 @@ exports.handler = async (
     };
   }
 
+  // デフォルト言語が英語なので、言語設定は必ず行う
+  const attributeFilter: AttributeFilter = {
+    AndAllFilters: [
+      {
+        EqualsTo: {
+          Key: '_language_code',
+          Value: {
+            StringValue: 'ja',
+          },
+        },
+      },
+    ],
+  };
+
+  // 絞り込み条件に沿って AttributeFilter を設定する
+  if (filters) {
+    filters.forEach((f) => {
+      if (f.attributeType === 'STRING_VALUE') {
+        attributeFilter.AndAllFilters?.push({
+          OrAllFilters: f.value.map((val) => ({
+            EqualsTo: {
+              Key: f.attributeKey,
+              Value: {
+                StringValue: val.toString(),
+              },
+            },
+          })),
+        });
+      } else if (f.attributeType === 'STRING_LIST_VALUE') {
+        attributeFilter.AndAllFilters?.push({
+          ContainsAny: {
+            Key: f.attributeKey,
+            Value: {
+              StringListValue: f.value.map((v) => v.toString()),
+            },
+          },
+        });
+      } else if (f.attributeType === 'LONG_VALUE') {
+        attributeFilter.AndAllFilters?.push({
+          OrAllFilters: f.value.map((val) => ({
+            EqualsTo: {
+              Key: f.attributeKey,
+              Value: {
+                LongValue: Number.parseFloat(f.value.toString()),
+              },
+            },
+          })),
+        });
+      } else if (f.attributeType === 'DATE_VALUE') {
+        attributeFilter.AndAllFilters?.push({
+          AndAllFilters: [
+            {
+              GreaterThanOrEquals: {
+                Key: f.attributeKey,
+                Value: {
+                  DateValue: new Date(f.value.toString()),
+                },
+              },
+            },
+            {
+              LessThanOrEquals: {
+                Key: f.attributeKey,
+                Value: {
+                  DateValue: new Date(f.value.toString()),
+                },
+              },
+            },
+          ],
+        });
+      }
+    });
+  }
+  console.log('AttributeFilter', JSON.stringify(attributeFilter));
+
   const kendra = new KendraClient({});
   const queryCommand = new QueryCommand({
     IndexId: INDEX_ID,
     QueryText: query,
-    AttributeFilter: {
-      EqualsTo: {
-        Key: '_language_code',
-        Value: {
-          StringValue: 'ja',
-        },
-      },
-    },
+    AttributeFilter: attributeFilter,
   });
 
   const queryRes = await kendra.send(queryCommand);
