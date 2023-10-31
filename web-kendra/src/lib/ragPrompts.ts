979d9dba11bfd1c1fc245cf38e9e457c1ba4e318
Kazuhito Go <gokazu@amazon.co.jp>
2023-10-31 22:38:32 +0900

    claude prompt 対応

--- web-kendra/src/lib/ragPrompts.ts
+++ web-kendra/src/lib/ragPrompts.ts
@@ -26,21 +26,24 @@ export const referenceParams = {
 };
 
 export const retrieveQueryPrompt = (queries: string[]) => {
-  return `Human: あなたは、文書検索で利用するQueryを生成するAIアシスタントです。
-以下の手順通りにQueryを生成してください。
-# Query生成の手順
-* 以下の「# Query履歴」の内容を全て理解してください。履歴は古い順に並んでおり、一番下が最新のQueryです。「# Query履歴END」がQuery履歴の終了を意味します。
-* 「要約して」などの質問ではないQueryは全て無視してください
-* 「〜って何？」「〜とは？」「〜を説明して」というような概要を聞く質問については、「〜の概要」と読み替えてください。
-* ユーザが最も知りたいことは、最も新しいQueryの内容です。最も新しいQueryの内容を元に、30トークン以内でQueryを生成してください。
-* 出力したQueryに主語がない場合は、主語をつけてください。主語の置き換えは絶対にしないでください。
-* 主語や背景を補完する場合は、「# Query履歴」の内容を元に補完してください。
-* Queryは「〜について」「〜を教えてください」「〜について教えます」などの語尾は絶対に使わないでください
-* 出力するQueryがない場合は、「No Query」と出力してください
-* 出力は生成したQueryだけにしてください。他の文字列は一切出力してはいけません。例外はありません。
-# Query履歴
+  return `
+
+Human: あなたは、文書検索で利用する Query を生成するAIアシスタントです。
+以下の <procedure></procedure> の xml タグに囲われた手順通りに Query を生成してください。
+<procedure>
+1. 以下の <query-history></query-history> の xml タグに囲われた Query 履歴の内容を全て理解してください。履歴は古い順に並んでおり、一番下が最新のQueryです。
+2. 「要約して」などの質問ではない Query は全て無視してください
+3. 「〜って何？」「〜とは？」「〜を説明して」というような概要を聞く質問については、「〜の概要」と読み替えてください。
+4. ユーザが最も知りたいことは、最も新しい Query の内容です。最も新しい Query の内容を元に、1 行で Query を生成してください。
+5. 出力した Query に主語がない場合は、主語をつけてください。主語の置き換えは絶対にしないでください。
+6. 主語や背景を補完する場合は、 <query-history></query-history> の xml タグに囲われた Query 履歴の内容を元に補完してください。
+7. Queryは「〜について」「〜を教えてください」「〜について教えます」などの語尾は絶対に使わないでください
+8. 出力する Query がない場合は、「No Query」と出力してください
+9. 出力は生成したQueryだけにしてください。他の文字列は一切出力してはいけません。例外はありません。
+</procedure>
+<query-history>
 ${queries.map((q) => `* ${q}`).join('\n')}
-# Query履歴END
+</query-history>
 Assistant: 
 `;
 };
@@ -49,52 +52,51 @@ export const basicPrompt = (
   referenceItems: RetrieveResultItem[],
   messages: Message[]
 ) => {
-  return `Human: あなたはユーザの質問に答えるAIアシスタントです。
-以下の手順でユーザの質問に答えてください。手順以外のことは絶対にしないでください。
-理解したら「I understand」とだけ返信してください。
-# 回答手順
-* 私が「Reference documents」というコメントをします。このコメントの後に、「参考ドキュメント」を連続して送信していきます。
-* あなたは「Reference documents」というコメントを受信したら、「Start」と返信してください。
-* 続いてJSON形式で「参考ドキュメント」を連続で投稿していきます。フォーマットは「# 参考ドキュメントのJSON形式」に示します。あなたはドキュメントを受信したら「Please continue」とだけ返信してください。
-* 私が全ての「参考ドキュメント」をすべて投稿したら「DONE」というコメントを送ります。あなたはこのコメントを受信したら「<< COMPLETED >>」と返信してください。
-* あなたの「OK」というコメントの後に、私が質問を投稿します。あなたは「# 回答のルール」に沿って質問にJSON形式で回答してください。ルールは絶対です。例外はありません。
-# 参考ドキュメントのJSON形式
+  return `
+
+Human: あなたは日本語をよく理解して回答する AI アシスタントです。
+Assistant: はい、そのとおりです。
+Human : 今から検索サービスから得た情報をいくつか渡すのでその情報だけを覚えてください。
+情報は <reference-information></reference-information> タグに囲まれた部分にあり複数あります。
+情報の一つ一つは、<info-x></info-x> という xml タグに囲まれます。
+info-x の x はインデックス番号で整数が入ります。
+情報には JSON が入っており、<information-format></information-format> という xml タグに囲まれたフォーマットで記述されています。
+<info-format>
 {
-  "DocumentId": "ドキュメントを一意に特定するIDです。",
-  "DocumentTitle": "ドキュメントのタイトルです。",
-  "DocumentURI": "ドキュメントが格納されているURIです。",
-  "Content": "ドキュメントの内容です。こちらをもとに回答してください。",
+  "DocumentId": ドキュメントの ID,
+  "DocumentTitle": ドキュメントのタイトル,
+  "DocumentURI": ドキュメントのURI,
+  "Content" ドキュメントの内容
 }
-# 回答のルール
-* 雑談や挨拶には応じないでください。「私はドキュメント検索を行う AI アシスタントです。雑談はできません。」とだけ出力してください。他の文言は一切出力しないでください。例外はありません。
-* 必ず「参考ドキュメント」をもとに回答してください。「参考ドキュメント」から読み取れないことは、絶対に回答しないでください。
-* 「参考ドキュメント」をもとに回答できない場合は、「回答に必要な情報が見つかりませんでした。」とだけ出力してください。例外はありません。
-* 質問に具体性がなく回答できない場合は、質問の仕方をアドバイスしてください。
-* 回答文以外の文字列は一切出力しないでください。回答はJSON形式ではなく、テキストで出力してください。見出しやタイトル等も必要ありません。
-Assistant: I understand
-Human: Reference documents
-Assistant: Start
+</info-format>
+Assistant: はい、わかりました。
+Human: <reference-information>
 ${referenceItems
-  .map((item) => {
-    return `Human: ${JSON.stringify({
+  .map((item,index) => {
+    return `<info-${index}> ${JSON.stringify({
       DocumentId: item.DocumentId,
       DocumentTitle: item.DocumentTitle,
       DocumentURI: item.DocumentURI,
       Content: item.Content,
-    })}
-Assistant: Please continue`;
+    })}</info-${index}>`;
   })
   .join('\n')}
-Human: DONE
-Assistant: << COMPLETED >>
+</reference-information>
+Assistant: はい、覚えました。
+Human: 今、覚えた情報だけを使って質問に回答してください。ただし、回答を出力するにあたって、以下の <rule></rule> で囲まれた回答のルールを必ず守ってください。
+<rule>
+* 今、覚えた情報だけを使ってください。今、覚えていない情報は絶対に使ってはいけません。例外はありません。
+* 覚えた情報で質問に回答できない場合は「回答に必要な情報が見つかりませんでした。」と出力してください。例外はありません。
+* 質問に具体性がなく回答できない場合は、質問の仕方をアドバイスしてください。
+* 回答文以外の文字列は一切出力しないでください。回答は JSON 形式ではなく、テキストで出力してください。見出しやタイトル等も出力してはいけません。例外はありません。
+* 質問は <question></question> の xml タグに囲われて渡されます。
+</rule>
+Assistant: 理解しました。ルールを必ず遵守します。質問はなんですか？
 ${messages
   .map((message) => {
-    return `${message.role === 'user' ? 'Human:' : 'Assistant: '} ${
-      message.content
-    }`;
+    return `${message.role === 'user' ? `Human: <question>${message.content}</question>` : `Assistant: ${message.content}`}`;
   })
-  .join('\n')}
-`;
+  .join('\n')}`;
 };
 
 export const referencedDocumentsPrompt = (
@@ -102,16 +104,18 @@ export const referencedDocumentsPrompt = (
   messages: Message[]
 ) => {
   return `${basicPrompt(referenceItems, messages)}
-Human: 先ほど回答の参考にした「参考ドキュメント」の情報を出力してください。
-以下の「# 回答の元になった参考ドキュメントのJSON形式」のフォーマットで出力してください。
-JSON以外の文字列は一才出力してはいけません。例外はありません。
-参考にしたドキュメントが複数ある場合は、配列で出力してください。
-# 回答の元になった参考ドキュメントのJSON形式
+Human: 先ほど回答を作成するにあたって使用した <reference-information></reference-information> の xml タグに囲われた情報を出力してください。
+以下の <reference-document-json></reference-document-json> の xml タグに囲われた JSON フォーマットで出力してください。
+<reference-document-json>
 {
     "DocumentId": "回答の参考にした「参考ドキュメント」のDocumentIdを記載してください。"
     "DocumentTitle": "回答の参考にした「参考ドキュメント」のDocumentTitleを記載してください。",
     "DocumentURI": "回答の参考にした「参考ドキュメント」のDocumentURIを記載してください。",
 }[]
+</reference-document-json>
+ただし JSON 以外の文字列は一切出力してはいけません。<reference-document-json></reference-document-json>  の xml のタグで囲ってもいけません。
+無機質に JSON だけを出力してください。例外はありません。
+参考にしたドキュメントが複数ある場合は、配列で出力してください。
 Assistant: 
 `;
 };
